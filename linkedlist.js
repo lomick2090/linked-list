c6f7693edc92aaf0289f44ad653e9d885e1c7674
minkman <lomick2090@gmail.com>
2023-01-30 14:35:17 -0600

    Add many functions

--- linkedlist.js
+++ linkedlist.js
@@ -25,7 +25,66 @@ function createList(head = null) { return {
         }
         return count;
     },
-    
+    tail: function tail() {
+        let node = this.head 
+        while (node.next){
+            node = node.next;
+        }
+        return node;
+    },
+    at: function at(index) {
+        let node = this.head 
+        for (let i = 0; i < index; i ++) {
+            if (node.next) {
+                node = node.next
+            } else {
+                return null;
+            }
+        }
+        return node;
+    },
+    pop: function pop() {
+        let l = this.size();
+        let current = this.head;
+        for (let i = 0; i < (l-2); i++) {
+            current = current.next
+        }
+        console.log(current.next)
+        current.next = null
+    },
+    contains: function contains(search) {
+        let current = this.head;
+        while (current) {
+            if (current.value == search) {
+                return true;
+            }
+            current = current.next
+        }
+        return false;
+    },
+    find: function find(search) {
+        let current = this.head;
+        let index = 0;
+        while (current) {
+            if (current.value == search) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return null;
+    },
+    toString: function toString() {
+        let string = '';
+        let current = this.head;
+        while(current) {
+            string = string + `( ${current.value} ) -> `
+            current = current.next;
+        } 
+        string = string + `null`;
+        return string;
+    }
+
 }};
 
 function createNode(value, next = null) { return {
@@ -38,7 +97,14 @@ list = createList();
 
 list.append('value 1');
 list.append('value 2');
-list.prepend('value 0')
-console.log(list.size())
+list.prepend('value 0');
+console.log(list.size());
+console.log(list.tail());
+console.log(list.at(2));
+console.log(list.contains('value 3'))
+console.log(list.find('value 2'), list.find('value 3'))
+console.log(list.toString())
+list.pop()
+list.pop()
 
-console.log(list)
+console.log(list.toString())
